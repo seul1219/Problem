0aee4c1eab521484a64d21afa47e08b3334f01f9
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-05-26 09:26:14 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 12772 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Main
+
+ {
+
+   public static void main(String args[])
+
+   { 
+
+     
+
+     Scanner sc = new Scanner(System.in);
+
+     
+
+     String str = sc.nextLine();
+
+     char c = str.charAt(0);
+
+     
+
+    System.out.println((int)c);
+
+   }
+
+ }
+

1f66c9674ea84d3c76427469d1ca83ff78ac603a
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-07-11 08:33:12 +0900

    [Bronze III] Title: 집 주소, Time: 72 ms, Memory: 11288 KB -BaekjoonHub

---
+++ 백준/Bronze/1284. 집 주소/집 주소.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+import java.io.*;
+
+ class Main
+
+ {
+
+   public static void main(String args[]) throws IOException
+
+   { 
+
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+     
+
+     StringBuilder sb = new StringBuilder();
+
+     
+
+     while(true) {
+
+       String tmp = br.readLine();
+
+       
+
+       if(tmp.equals("0")) {
+
+         break;
+
+       } else {
+
+         int res = tmp.length()-1+2;
+
+         int[] arr = new int[tmp.length()];
+
+         for(int i=0; i<tmp.length(); i++) {
+
+           arr[i] = tmp.charAt(i)-'0';
+
+           
+
+           if(arr[i]==1) {
+
+             res += 2;
+
+           } else if(arr[i]==0) {
+
+             res += 4;
+
+           } else {
+
+             res += 3;
+
+           }
+
+         }
+
+         
+
+         sb.append(res).append("\n");
+
+       }
+
+     }
+
+     
+
+     System.out.println(sb.toString());
+
+   }
+
+ }
+

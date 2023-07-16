d67675a61828aa91285dffdd0d6428bae80cc942
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-07-16 13:37:07 +0900

    [Bronze V] Title: N 찍기, Time: 168 ms, Memory: 17144 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+import java.io.*;
+
+ class Main
+
+ {
+
+   public static void main(String args[]) throws IOException
+
+   { 
+
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+     StringBuilder sb = new StringBuilder();
+
+     
+
+     int N = Integer.parseInt(br.readLine());  
+
+     for(int i=1; i<=N; i++) {
+
+       sb.append(i).append("\n");
+
+     }
+
+     
+
+     System.out.println(sb.toString());
+
+   }
+
+ }
+

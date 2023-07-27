cb28a2eb97c7ee38d97302c877e61bdaf0d6ae4f
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-07-27 22:27:58 +0900

    Update 긴자리 계산.java

--- 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.math.BigInteger;
 
 public class Main {
- 
+  
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 	 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

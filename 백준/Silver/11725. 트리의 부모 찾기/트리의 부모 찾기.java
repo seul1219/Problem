cfdd68c39c0ac6c1848d8aea7e6e5146f05ac909
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-08-24 22:44:48 +0900

    Update 트리의 부모 찾기.java

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
     
    public static void main(String args[]) throws IOException { 

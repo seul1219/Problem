1bdea794034b3ea730680d2943aa5d4bd260d4a6
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-08-29 23:26:27 +0900

    Update 한동이는 공부가 하기 싫어！.java

--- 백준/Silver/3182. 한동이는 공부가 하기 싫어！/한동이는 공부가 하기 싫어！.java
+++ 백준/Silver/3182. 한동이는 공부가 하기 싫어！/한동이는 공부가 하기 싫어！.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-
+  
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {

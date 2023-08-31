d8f00313c4a3b414836ae4391946a40fcbc0e677
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-08-31 22:40:55 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 80 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i=N; i>=0; i--) {
+			int j = 0;
+			while(j!=i) {
+				sb.append("*");
+				j++;
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}

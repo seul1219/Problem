1aa4e120f95068d09a0179a52ebf306c4139f9df
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-09-08 23:09:10 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 76 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+import java.io.BufferedReader;
+
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+
+public class Main {
+
+   public static void main(String args[]) throws IOException { 
+
+     
+
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+     
+
+     StringTokenizer st = new StringTokenizer(br.readLine());
+
+     int[] num = new int[3];
+
+     num[0] = Integer.parseInt(st.nextToken());
+
+     num[1] = Integer.parseInt(st.nextToken());
+
+     num[2] = Integer.parseInt(st.nextToken());
+
+     
+
+     Arrays.sort(num);
+
+     
+
+     System.out.println(num[2]+num[1]);
+
+    }
+
+ }
+

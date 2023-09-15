915d102d078d5bc44140414bb57c1cc66a074b44
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-09-15 22:49:31 +0900

    Update 별 찍기 － 4.java

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+		  
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());

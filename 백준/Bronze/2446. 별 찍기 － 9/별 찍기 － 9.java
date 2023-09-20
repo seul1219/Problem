d62ef17466b74eeab46f3006488b8e166709c5be
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-09-20 23:17:24 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 84 ms, Memory: 11736 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+
+public class Main {
+
+   public static void main(String args[]) throws IOException { 
+
+     
+
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+     int N = Integer.parseInt(br.readLine());
+
+     
+
+     StringBuilder sb = new StringBuilder();
+
+     int left = 0;
+
+     int right = 2*N-1;
+
+     int end = 2*N-1;
+
+     int mid = (2*N-1)/2;
+
+     for(int i=0; i<end; i++) {
+
+       if(left>0) {
+
+         for(int j=0; j<left; j++) {
+
+           sb.append(" ");
+
+         }
+
+         for(int j=left; j<right; j++) {
+
+           sb.append("*");
+
+         }
+
+         
+
+       } else {
+
+         for(int j=left; j<right; j++) {
+
+           sb.append("*");
+
+         }
+
+         
+
+       }
+
+       
+
+       sb.append("\n");
+
+       if(i<mid) {
+
+         left++;
+
+         right--;
+
+       } else {
+
+         left--;
+
+         right++;
+
+       }
+
+       
+
+     }
+
+     
+
+     System.out.println(sb.toString());
+
+    }
+
+ }
+

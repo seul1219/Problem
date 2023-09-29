a5388fda2f38779d52024701a9c22049144dcf38
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-09-30 01:03:43 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 80 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+   public static void main(String args[]) throws IOException { 
+
+     
+
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+     
+
+     int N = Integer.parseInt(br.readLine());
+
+     int one = N/11;
+
+     
+
+     System.out.println(one*10);   
+
+   }
+
+}

681c3290f63bfb8d018fa833614cce8fa7a5defe
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-10-02 23:18:11 +0900

    [Bronze IV] Title: 과자, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,24 @@
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		if(K*N>M) {
+			System.out.println((K*N)-M);
+		} else {
+			System.out.println("0");
+		}
+	}
+}

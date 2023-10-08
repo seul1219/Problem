79d78d10a78ae7138ba2aefe960efddeaa259e07
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-10-08 20:38:31 +0900

    Update 과자.java

--- 백준/Bronze/10156. 과자/과자.java
+++ 백준/Bronze/10156. 과자/과자.java
@@ -2,7 +2,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.io.BufferedReader;
 import java.io.IOException;
-      
+         
 public class Main {
    
 	public static void main(String[] args) throws IOException {

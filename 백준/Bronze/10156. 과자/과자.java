a246c581c701ed26a42180e53e5f82b84354b340
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-10-09 23:23:32 +0900

    Update 과자.java

--- 백준/Bronze/10156. 과자/과자.java
+++ 백준/Bronze/10156. 과자/과자.java
@@ -10,7 +10,7 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+		   
 		int K = Integer.parseInt(st.nextToken());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());

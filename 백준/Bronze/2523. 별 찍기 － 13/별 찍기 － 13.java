d182d0b41c222cca7bc07df62c9086c5efa85e56
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-10-12 23:03:14 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 80 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+
+   public static void main(String args[]) throws IOException { 
+     
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     
+     int N = Integer.parseInt(br.readLine());
+     StringBuilder sb = new StringBuilder();
+     int left = 1;
+     for(int i=0; i<2*N-1; i++) {
+       for(int j=0; j<left; j++) {
+         sb.append("*");
+       }
+       sb.append("\n");
+       if(i>=N-1) {
+         left--;
+       } else {
+         left++;
+       }
+     }
+     
+     System.out.println(sb.toString());
+   }
+}

331a0e3022f6dd2a72d06a1957a4788602130a8b
seul1219 <125745601+seul1219@users.noreply.github.com>
2023-10-14 21:37:14 +0900

    Update 별 찍기 － 1.java

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
-		
+		    
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
 		int N = sc.nextInt();

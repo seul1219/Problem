b030c7c5a937f8bc5d3c336fc6ca102bb94e6ed5
seul1219 <125745601+seul1219@users.noreply.github.com>
2024-01-01 19:12:09 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 72 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,41 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int[] arr = new int[9];
-		
-		int sum = 0;
-		for(int i=0; i<9; i++) {
-			arr[i] = sc.nextInt();
-			sum += arr[i];
-		}
-		
-		Arrays.sort(arr);
-		
-		int d1 = 0;
-		int d2 = 0;
-		L: for(int i=0; i<9; i++) {
-			for(int j=i+1; j<9; j++) {
-				int tmp = sum - arr[i] - arr[j];
-			
-				if(tmp == 100) {
-					d1 = i;
-					d2 = j;
-					
-					break L;
-				}
-			}
-		}
-		
-		for(int i=0; i<9; i++) {
-			if(i!=d1 && i!=d2) {
-				System.out.println(arr[i]);
-			}
-		}
-	}
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int sum = 0;
+        for(int i=0; i<9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        int ans = sum-100;
+        Arrays.sort(arr);
+
+        int ans1 = 0;
+        int ans2 = 0;
+       R: for(int i=0; i<8; i++) {
+            for(int j=i+1; j<9; j++) {
+
+                if(arr[i] + arr[j] == ans) {
+                    ans1 = i;
+                    ans2 = j;
+                    break R;
+                }
+            }
+        }
+
+        for(int i=0; i<9; i++) {
+            if(i!=ans1 && i!=ans2) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
 }

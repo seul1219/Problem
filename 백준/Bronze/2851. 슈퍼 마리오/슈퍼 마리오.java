ab522a3718d959bfacb17ab5287edecfaa5f2223
seul1219 <125745601+seul1219@users.noreply.github.com>
2024-01-01 19:28:32 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 76 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,36 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int[] arr = new int[10];
-		
-		for(int i=0; i<arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		int sum = 0;
-		int idx = 0;
-		while(idx<arr.length && sum<=100) {
-			sum+=arr[idx];
-			idx++;
-		}
-		
-		int sum2 = 0;
-		for (int i=0; i<idx-1; i++) {
-		    sum2 += arr[i];
-		}
-		
-		int res = 0;
-		if(Math.abs(100-sum) == Math.abs(100-sum2)) {
-			res = sum;
-		} else {
-			res = Math.abs(100-sum) > Math.abs(100-sum2) ? sum2 : sum;
-		}
-		
-		System.out.println(res);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[10];
+        int sum = 0;
+        int[] sumArr = new int[10];
+        for(int i=0; i<10; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+            sumArr[i] = sum;
+        }
+
+        int[] adjArr = new int[10];
+        for(int i=0; i<10; i++) {
+            adjArr[i] = sumArr[i]-100;
+            if(adjArr[i]<0) {
+                adjArr[i] = adjArr[i] * -1;
+            }
+        }
+
+        int ans = Integer.MAX_VALUE;
+        int num = 0;
+        for(int i=0; i<10; i++) {
+            if(adjArr[i]<=ans) {
+                ans = adjArr[i];
+                num = i;
+            }
+        }
+
+        System.out.println(sumArr[num]);
+    }
 }

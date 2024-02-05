5e245c47a2d7177ee3f5ac70a36869a153a0183c
seul1219 <125745601+seul1219@users.noreply.github.com>
2024-02-05 23:55:02 +0900

    [Silver III] Title: N과 M (3), Time: 364 ms, Memory: 111836 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -4,42 +4,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	
 	public static int N;
 	public static int M;
-	public static int[] arr;
-	public static int[] sel;
 	public static StringBuilder sb = new StringBuilder();
+	public static int[] sel;
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-
-		arr = new int[N];
-		for(int i=0; i<N; i++) {
-			arr[i] = i+1;
-		}
 		
 		sel = new int[M];
-		select(0);
+		NM(0);
+		
 		System.out.println(sb.toString());
 	}
-
-	public static void select(int cnt) {
+	
+	public static void NM(int cnt) {
+		
 		if(cnt==M) {
 			for(int i=0; i<M; i++) {
 				sb.append(sel[i]).append(" ");
 			}
 			sb.append("\n");
+			
 			return;
 		} else {
-			for(int i=0; i<N; i++) {
-				sel[cnt] = arr[i];
-				select(cnt+1);
+			for(int i=1; i<=N; i++) {
+					sel[cnt] = i;
+					NM(cnt+1);
 			}
 		}
 	}

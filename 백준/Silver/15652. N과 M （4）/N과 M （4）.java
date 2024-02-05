51b49e42cc6bd3e2649a00dbe9aed71b50189e7f
seul1219 <125745601+seul1219@users.noreply.github.com>
2024-02-05 23:50:14 +0900

    [Silver III] Title: N과 M (4), Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static int N;
+	public static int M;
+	public static StringBuilder sb = new StringBuilder();
+	public static int[] sel;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		sel = new int[M];
+		NM(0, 1);
+		
+		System.out.println(sb.toString());
+	}
+	
+	public static void NM(int cnt, int start) {
+		
+		if(cnt==M) {
+			for(int i=0; i<M; i++) {
+				sb.append(sel[i]).append(" ");
+			}
+			sb.append("\n");
+			
+			return;
+		} else {
+			for(int i=start; i<=N; i++) {
+					sel[cnt] = i;
+					NM(cnt+1, i);
+			}
+		}
+	}
+}

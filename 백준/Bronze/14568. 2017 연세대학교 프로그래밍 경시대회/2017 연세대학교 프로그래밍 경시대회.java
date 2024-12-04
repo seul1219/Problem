80ee4f5859dfd6e7d3933e5f1d39fcdfbf9449d3
seul1219 <125745601+seul1219@users.noreply.github.com>
2024-12-04 18:10:18 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 72 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
@@ -1,48 +1,22 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int num = 0;
+        for(int i=1; i<N; i++) {
+            for(int j=3; j<N; j++) {
+                for(int k=1; k<N; k++) {
+                    if (N - (i+j+k)==0 && j>=i+2 && k%2==0) {
+                        num++;
+                    }
+                }
+            }
+        }
 
-  
-
-   public static void main(String args[]) throws IOException { 
-
-     
-
-     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-     
-
-     int N = Integer.parseInt(br.readLine());
-
-     
-
-     int cnt = 0;
-
-     for(int i=1; i<=N-2; i++) {
-
-       for(int j=1; j<=N-1; j++) {
-
-         for(int k=1; k<=N; k++) {
-
-           
-
-           if(j>=i+2 && k%2==0 && i+j+k==N) {
-
-             cnt++;
-
-           }
-
-         }
-
-       }      
-
-     }
-
-     
-
-     System.out.println(cnt);
-
+        System.out.println(num);
     }
-
- }
-
+}

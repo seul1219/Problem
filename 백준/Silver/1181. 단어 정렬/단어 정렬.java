22075ee6fc0d5e114d9c6cce687f7b449523534c
seul1219 <125745601+seul1219@users.noreply.github.com>
2025-06-03 20:40:04 +0900

    [Silver V] Title: 단어 정렬, Time: 340 ms, Memory: 26828 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,50 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    
     public static void main(String[] args) throws IOException {
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
         int N = Integer.parseInt(br.readLine());
         
-        String[] arr = new String[N];
-        for(int i=0; i<N; i++) {
-            arr[i] = br.readLine();
+        Set<String> set = new HashSet<>();
+        for (int i=0; i<N; i++) {
+            set.add(br.readLine());
         }
         
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-            	//길이가 같으면
-                if(s1.length() == s2.length()) {
-                    for (int i = 0; i < s1.length(); i++) {
-                        char c1 = s1.charAt(i);
-                        char c2 = s2.charAt(i);
-                        if (c1 != c2) {
-                        	//사전 순
-                            return c1 - c2;
-                        }
-                    }
-                    //동일하면
-                    return 0;
-                }
-                //길이 짧은 순
+        List<String> list = new ArrayList<>(set);
+        list.sort((s1, s2) -> {
+            if(s1.length()!=s2.length()) {
                 return s1.length() - s2.length();
+            } else {
+                return s1.compareTo(s2);
             }
         });
         
-        for (int i = 0; i < N; i++) {
-            if (i == 0 || !arr[i].equals(arr[i - 1])) {
-                sb.append(arr[i]).append("\n");
-            }
+        for (String s : list) {
+            sb.append(s).append("\n");
         }
-        
+
         System.out.print(sb.toString());
     }
 }
